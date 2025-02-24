e0b3c1998df016cf1694737d877e0fbe3c9ad7b0
parthsapariya745 <parthsapariyait7@gmail.com>
2025-02-24 15:15:58 +0530

    update

--- electricity-bill/1.cpp
+++ electricity-bill/1.cpp
@@ -2,21 +2,39 @@
 using namespace std;
 
 int main(){
-    int user;
+    int user,add = 0,sum = 0;
+    float sub = 0,total = 0;
 
-    cout << "Enter any number: ";
+    cout << "Enter your unit: ";
     cin >> user;
 
-    if (user < 0)
+    cout << "-----------------------------" << endl;
+
+    if (user > 0 && user <= 50)
     {
-        cout << "The number is Negative";
+        add = user * 0.50;
     }
-    else if (user == 0)
-    {
-        cout << "The number is Neutral";
+    
+    else if(user > 50 && user <= 150){
+        add = 25;
+        sub = (user - 50) * 0.75;        
+    }  
+
+    else if(user > 150 && user <= 300){
+        add = 25 + 75; 
+        sub = (user - 150) * 1.20;
     }
-    else
-    {
-        cout << "The number is Positive";
+
+    else{
+        add = 25 + 75 + 180;
+        sub = (user - 300) * 1.50;
     }
+
+    sum = sub + add;
+    cout << "total amount 0% tax: " << sum << endl;
+
+    cout << "-----------------------------" << endl;
+
+    total = sum + (sum * 0.20);
+    cout << "total amount +20% tax: " << total << endl;
 }
